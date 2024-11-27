c0ac0aebf52b448ae2ade543a0c18f9ed47e8b8a
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-11-27 12:29:24 +0700

    fix

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM eclipse-temurin:21-jdk-jammy
-WORKDIR /app
-COPY . .
-RUN ./gradlew bootJar
-CMD ["java", "-jar", "build/libs/hhRecruiter-0.0.1.jar"]
--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-docker stop hhRecruiter
-docker rm hhRecruiter
-docker rmi hhRecruiter
-docker build -t hhRecruiter .
-docker run -d --name hhRecruiter -p 2000:2000 hhRecruiter
-docker logs -f hhRecruiter
+
+kill -9 $(ps aux | grep "java -jar build/libs/hhRecruiter-0.0.1.jar" | grep -v grep | awk '{print $2}')
+
+nohup java -jar build/libs/hhRecruiter-0.0.1.jar > build/resources/loger.txt 2>&1 &
