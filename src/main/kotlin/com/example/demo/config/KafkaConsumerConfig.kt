b96b97cc18aab955cf947da79739ad8b4fb0934d
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-12-05 13:33:20 +0700

    fix

--- src/main/kotlin/com/example/demo/config/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/example/demo/config/KafkaConsumerConfig.kt
@@ -2,6 +2,7 @@ package com.example.demo.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -12,10 +13,14 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 @EnableKafka
 @Configuration
 class KafkaConsumerConfig {
+
+    @Value("\${kafka.bootstrap-servers}")
+    private val bootstrapServers: String=""
+
     @Bean
     fun consumerFactoryApi(): ConsumerFactory<String, String> {
         val configProps: MutableMap<String, Any> = HashMap()
-        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "37.194.168.90:9092"
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] =bootstrapServers
         configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.GROUP_ID_CONFIG] = "hh"
@@ -32,7 +37,7 @@ class KafkaConsumerConfig {
     @Bean
     fun consumerFactoryAuth(): ConsumerFactory<String, String> {
         val configProps: MutableMap<String, Any> = HashMap()
-        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "37.194.168.90:9092"
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
         configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         configProps[ConsumerConfig.GROUP_ID_CONFIG] = "auth_service"
--- src/main/kotlin/com/example/demo/config/KafkaProducerConfig.kt
+++ src/main/kotlin/com/example/demo/config/KafkaProducerConfig.kt
@@ -2,6 +2,7 @@ package com.example.demo.config
 
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -12,10 +13,13 @@ import org.springframework.kafka.core.ProducerFactory
 @EnableKafka
 @Configuration
 class KafkaProducerConfig {
+    @Value("\${kafka.bootstrap-servers}")
+
+    private val bootstrapServers: String=""
     @Bean
     fun producerFactory(): ProducerFactory<String, String> {
         val configProps: MutableMap<String, Any> = HashMap()
-        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "37.194.168.90:9092"
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
         configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         return DefaultKafkaProducerFactory(configProps)
